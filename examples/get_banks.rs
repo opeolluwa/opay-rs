88f5895cfa2a3c8c7304f1df1d0af20e6ab3a346
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-10-02 10:54:50 +0100

    chore: add example

---
+++ examples/get_banks.rs
@@ -0,0 +1,17 @@
+use opay_rs::{
+    opay_client::OpayClient,
+    error::OpayClientError,
+    opay::{Environment, MerchantId, PublicKey},
+};
+
+#[tokio::main]
+async fn main() -> Result<(), OpayClientError> {
+    let env = Environment::Development; // Environment::Default
+    let public_key = PublicKey::from(&std::env::var("OPAY_PUBLIC_KEY").ok().unwrap());
+    let merchant_id = MerchantId::from(&std::env::var("OPAY_MERCHANT_ID").ok().unwrap());
+    let opay_client = OpayClient::new(env, public_key, merchant_id);
+
+    let bank_list = opay_client.get_bank_list().await;
+
+    Ok(())
+}
